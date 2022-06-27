9ead04ca3afb028a697dd794f4b91e6ee28d949d
lyy <1139707026@qq.com>
2022-06-27 11:25:03 +0800

    第一次

--- src/com/lyy/pojo/User.java
+++ src/com/lyy/pojo/User.java
@@ -1,4 +1,16 @@
 package com.lyy.pojo;
 
 public class User {
+    private String username;
+    private String password;
+    private String gender;
+
+    public User(String username, String password, String gender) {
+        this.username = username;
+        this.password = password;
+        this.gender = gender;
+    }
+
+    public User() {
+    }
 }
